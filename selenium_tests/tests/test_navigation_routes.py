e23542f64b73defa6005f7ecc6650a49dd03125e
Navya <navya.21feb@gmail.com>
2025-07-14 13:42:27 +0530

    checked and fixed files

--- selenium_tests/tests/test_navigation_routes.py
+++ selenium_tests/tests/test_navigation_routes.py
@@ -11,7 +11,7 @@ class TestNavigation:
     @classmethod
     def setup_class(cls):
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
+        # chrome_options.add_argument("--headless")
         chrome_options.add_argument("--window-size=1920,1080")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
